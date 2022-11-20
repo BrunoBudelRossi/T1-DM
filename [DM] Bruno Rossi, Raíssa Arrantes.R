8921b30e84fad1970f30e30803fdb8028648ea19
BrunoBudelRossi <brunobudelrossi3745@outlook.com>
2022-11-20 19:26:56 -0300

    feat: make results dataframe

--- [DM] Bruno Rossi, Raíssa Arrantes.R
+++ [DM] Bruno Rossi, Raíssa Arrantes.R
@@ -1,8 +1,51 @@
-setwd("C:\\Users\\bruno\\OneDrive - NuageIT\\Área de Trabalho\\Mineração de Dados")
+setwd("C:\\Users\\bruno\\OneDrive - NuageIT\\Área de Trabalho\\Mineração de Dados\\T1---DM")
+
+getwd()
 
 df1 <- read.csv("./_ASSOC_BGFriends_01.csv")
 
 df2 <- read.csv("./_ASSOC_BGFriends_02.csv")
 
-# concatena 2 data frames por linha (rows)
 df <- rbind(df1, df2)
+
+columns= c("names","wins", "defeats") 
+results = data.frame(matrix(nrow = 0, ncol = length(columns))) 
+colnames(results) = columns
+
+flag <- TRUE
+for (i in 1:nrow(df)) {
+  row <- df[i,]
+  
+  isWin <- row$Amigos > row$Oponentes
+  
+  name <- gsub(" ","", row$Jogadore.a.s)
+  
+  if (nrow(results) != 0) {
+    for (j in 1:nrow(results)) {
+      row2 <- results[j,]
+      if (name == row2$names) {
+        if (isWin) {
+          results[j, 2] <- as.numeric(results[j, 2]) + 1
+        } else {
+          results[j, 3] <- as.numeric(results[j, 3]) + 1
+        }
+        flag <- FALSE
+      }
+    }
+  }
+  
+  if (flag) {
+    if (isWin) {
+      results[nrow(results) + 1,] = c(name, 1, 0)
+    } else {
+      results[nrow(results) + 1,] = c(name, 0, 1)
+    }
+  }
+  flag <- TRUE
+}
+results$wins <- as.numeric(as.character(results$wins))
+results$defeats <- as.numeric(as.character(results$defeats))
+orderResults <- results[order(results$wins, decreasing = TRUE),]
+
+
+
